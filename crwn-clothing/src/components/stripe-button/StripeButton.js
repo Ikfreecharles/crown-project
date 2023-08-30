c6738c9997f55af77cfc0512dbb04b62c916f815
Charles Ikulayo <sleektrends7@gmail.com>
2023-08-30 20:48:55 +0200

    updated the dependencies in the FE

--- crwn-clothing/src/components/stripe-button/StripeButton.js
+++ crwn-clothing/src/components/stripe-button/StripeButton.js
@@ -1,26 +1,42 @@
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { clearCart } from "../../redux/cart/Cart.action";
+import { selectCartItems } from "../../redux/cart/Cart.selector";
 
-function StripeButton({ price }) {
-   const priceForStripe = price * 100;
-   const PUBLISHABLE_KEY =
-      "pk_test_51JaYgiLReOHxoLxPArxpRf7Sbw7w4PY3GyVBWip5J22nmH3Y9TCWWsUfOL0Xv6WxSPoOyJWG1kdqi3mKEzF2kZMf00H7GMKuNe";
-   const onToken = (token) => {
-      console.log(token);
-      alert("Payment successful");
-   };
-   return (
-      <StripeCheckout
-         label="Pay Now"
-         name="CRWN CLOTHING Ltd"
-         billingAddress
-         shippingAddress
-         description={`Your total is ${price}`}
-         amount={priceForStripe}
-         panelLabel="Pay Now"
-         token={onToken}
-         stripeKey={PUBLISHABLE_KEY}
-      />
-   );
+function StripeButton({ price, clearCart }) {
+  const priceForStripe = price * 100;
+  const PUBLISHABLE_KEY =
+    "pk_test_51JaYgiLReOHxoLxPArxpRf7Sbw7w4PY3GyVBWip5J22nmH3Y9TCWWsUfOL0Xv6WxSPoOyJWG1kdqi3mKEzF2kZMf00H7GMKuNe";
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment successful");
+    clearCart();
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN CLOTHING Ltd"
+      billingAddress
+      shippingAddress
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={PUBLISHABLE_KEY}
+    />
+  );
 }
 
-export default StripeButton;
+const mapStateToProps = createStructuredSelector({
+  cartItem: selectCartItems,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  clearCart: () => {
+    dispatch(clearCart());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(StripeButton);
--- crwn-clothing/src/redux/cart/Cart.reducer.js
+++ crwn-clothing/src/redux/cart/Cart.reducer.js
@@ -2,44 +2,47 @@ import { CartActionTypes } from "./Cart.types";
 import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
-   cartItems: [],
-   hidden: true,
+  cartItems: [],
+  hidden: true,
 };
 
 const CartReducer = (currentState = INITIAL_STATE, action) => {
-   switch (action.type) {
-      case CartActionTypes.TOGGLE_CART_HIDDEN:
-         return {
-            ...currentState,
-            hidden: !currentState.hidden,
-         };
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...currentState,
+        hidden: !currentState.hidden,
+      };
 
-      case CartActionTypes.ADD_ITEM:
-         return {
-            ...currentState,
-            cartItems: addItemToCart(currentState.cartItems, action.payload),
-         };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...currentState,
+        cartItems: addItemToCart(currentState.cartItems, action.payload),
+      };
 
-      case CartActionTypes.CLEAR_ITEM_FROM_CART:
-         return {
-            ...currentState,
-            cartItems: currentState.cartItems.filter(
-               (cartItem) => cartItem.id !== action.payload.id
-            ),
-         };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...currentState,
+        cartItems: currentState.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
 
-      case CartActionTypes.REMOVE_ITEM:
-         return {
-            ...currentState,
-            cartItems: removeItemFromCart(
-               currentState.cartItems,
-               action.payload
-            ),
-         };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...currentState,
+        cartItems: removeItemFromCart(currentState.cartItems, action.payload),
+      };
 
-      default:
-         return currentState;
-   }
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...currentState,
+        cartItems: [],
+      };
+
+    default:
+      return currentState;
+  }
 };
 
 export default CartReducer;
